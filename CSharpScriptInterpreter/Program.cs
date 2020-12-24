8fa61e61da1468ce1b1885cf6665a908cfada66e
若汝棋茗 <505554090@qq.com>
2020-12-24 15:17:28 +0800

    修改指令
    修改

--- CSharpScriptInterpreter/Program.cs
+++ CSharpScriptInterpreter/Program.cs
@@ -2,6 +2,7 @@
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,18 @@ namespace CSharpScriptInterpreter
         {
             if (args.Length > 0)
             {
-                Run(args);
+                foreach (string arg in args)
+                {
+                    if (File.Exists(arg))
+                    {
+                        Run(arg);
+                    }
+                    else
+                    {
+                        Order(arg);
+                    }
+                }
+
             }
             else
             {
@@ -26,32 +38,51 @@ namespace CSharpScriptInterpreter
 
         }
 
-        static void Run(params string[] paths)
+        static void Run(string path)
         {
-            foreach (string path in paths)
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("执行开始");
+            CodeProvider codeProvider = new CodeProvider();
+            codeProvider.Path = path;
+
+            string mes;
+            if (!codeProvider.PreviewRun(out mes))
             {
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-                Console.WriteLine("执行开始");
-                CodeProvider codeProvider = new CodeProvider();
-                codeProvider.Path = path;
-
-                string mes;
-                if (!codeProvider.PreviewRun(out mes))
-                {
-                    Console.WriteLine(mes);
-                    return;
-                }
-
-                if (!codeProvider.Run(out mes))
-                {
-                    Console.WriteLine(mes);
-                    return;
-                }
-                stopwatch.Stop();
-                Console.WriteLine("执行结束,用时：{0}ms",stopwatch.Elapsed.TotalMilliseconds);
-                Console.WriteLine();
+                Console.WriteLine(mes);
+                return;
             }
+
+            if (!codeProvider.Run(out mes))
+            {
+                Console.WriteLine(mes);
+                return;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("执行结束,用时：{0}ms", stopwatch.Elapsed.TotalMilliseconds);
+            Console.WriteLine();
+
+        }
+
+        static void Order(string orderString)
+        {
+            switch (orderString)
+            {
+                case "-":
+                    {
+                        ConsoleWriteLine("我爱你们",ConsoleColor.Magenta);
+                        break;
+                    }
+                default:ConsoleWriteLine("指令无效",ConsoleColor.Red);
+                    break;
+            }
+        }
+
+        static void ConsoleWriteLine(string mes,ConsoleColor consoleColor= ConsoleColor.White)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(mes);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
